f86cf4675db4b6bc230c2e64f14779cc26821690
Francisco Chaves <francisco.jschaves@gmail.com>
2019-02-21 20:42:13 -0300

    Implementando navegação entre activities

--- app/src/main/java/br/com/franciscochaves/festafimdeano/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/festafimdeano/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.franciscochaves.festafimdeano;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
         if(id == R.id.button_confirm){
             //Lógica de navegação
+            Intent intent = new Intent(this, DetailsActivity.class);
+            startActivity(intent);
         }
 
     }
