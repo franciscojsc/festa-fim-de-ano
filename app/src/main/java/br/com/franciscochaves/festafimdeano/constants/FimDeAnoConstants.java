da7bc4b7a2fdd409d770eb0911ae714bd94b1ab7
Francisco Chaves <francisco.jschaves@gmail.com>
2019-02-21 21:20:48 -0300

    Classe responsável por definir algumas constantes do projeto

--- app/src/main/java/br/com/franciscochaves/festafimdeano/constants/FimDeAnoConstants.java
+++ app/src/main/java/br/com/franciscochaves/festafimdeano/constants/FimDeAnoConstants.java
@@ -1,4 +1,9 @@
 package br.com.franciscochaves.festafimdeano.constants;
 
 public class FimDeAnoConstants {
+
+    public static final String PRESENCE = "presence_key";
+    public static final String CONFIRMED_WILL_GO = "yes";
+    public static final String CONFIRMED_WONT_GO = "no";
+
 }
